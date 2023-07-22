16775863741b733b78caff79214f7364bdce81a6
ReXerses <108536533+ReXerses@users.noreply.github.com>
2023-07-23 01:09:36 +0200

    Aggiunta la difficoltà Impossibile tramite algoritmo minimax

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let player1, player2;
-let mosseDisponibili = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+let mosseDisponibiliGl = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 let sceltaFlag; // flag per conservare la scelta tra secondo player oppure Bot
 let turnoAttuale = 1; // 1 primo giocatore , 2 secondo giocatore
 
 const game = (() => {
     const sceltePlayer = document.querySelector('.scelte-player');
     const versusPlayer = document.getElementById(101);
-    const versusBot = document.getElementById(102);
+    const versusBotFacile = document.getElementById(102);
+    const versusBotImpossible = document.getElementById(103);
+
 
     const player = (nome, segno) => {
         const mosse = [];
@@ -15,9 +17,14 @@ const game = (() => {
         return { nome , segno, mosse, punteggio, turno};
     };
 
-    function getControBot () {
-        const versusBot = document.getElementById(102);
-        return versusBot;
+    function getControBotFacile () {
+        const versusBotFacile = document.getElementById(102);
+        return versusBotFacile;
+    }
+
+    function getControBotImpossibile () {
+        const versusBotImpossible = document.getElementById(103);
+        return versusBotImpossible;
     }
 
     function getControPlayer () {
@@ -73,7 +80,7 @@ const game = (() => {
         const player1Symbol = formData.get('player1Symbol');
         let player2Name = formData.get('player2Name');
 
-        if (sceltaFlag === 1) {
+        if (sceltaFlag === 1 || sceltaFlag === 2) {
             player2Name = 'Bot';
         }
         const player2Symbol = (player1Symbol === 'X') ? 'O' : 'X';
@@ -134,9 +141,9 @@ const game = (() => {
             }
 
             player1.mosse.push(parseInt(casella.id));
-            const index = mosseDisponibili.indexOf(parseInt(casella.id));
+            let index = mosseDisponibiliGl.indexOf(parseInt(casella.id));
             if (index > -1) {
-            mosseDisponibili.splice(index, 1);
+                mosseDisponibiliGl.splice(index, 1);
             }
 
             if (controlloVittoria(player1)) {
@@ -179,6 +186,10 @@ const game = (() => {
             }
 
             player2.mosse.push(parseInt(casella.id));
+            index = mosseDisponibiliGl.indexOf(parseInt(casella.id));
+            if (index > -1) {
+                mosseDisponibiliGl.splice(index, 1);
+            }
             if (controlloVittoria(player2)) {
                 const modal = document.getElementById('modal');
                 const winnerText = document.getElementById('winner-text');
@@ -197,14 +208,84 @@ const game = (() => {
 /* ------------------  Funzione modalità facile contro il bot (mosse random) ---------------- */
     function mossaBotFacile () {
 
-        const randomIndex = Math.floor(Math.random() * mosseDisponibili.length);
-        const botMove = mosseDisponibili[randomIndex];
+        const randomIndex = Math.floor(Math.random() * mosseDisponibiliGl.length);
+        const botMove = mosseDisponibiliGl[randomIndex];
 
-        const botCasella = document.getElementById(botMove.toString());
-        botCasella.click();
-        mosseDisponibili.splice(randomIndex, 1);
+        const botFacileCasella = document.getElementById(botMove.toString());
+        botFacileCasella.click();
 
     }
+
+/* ------------------ funzione modalità impossibile contro bot (mosse utilizzando l'algoritmo Minimax)  ----------- */
+    function getMosseDisponibili () {
+        return mosseDisponibiliGl.slice();
+    }
+
+
+    function minimax(giocatoreMax, giocatoreMin, mosseDisponibiliFor) {
+      if (controlloVittoria(giocatoreMax)) {
+        return 1; // Il giocatoreMax ha vinto
+      } else if (controlloVittoria(giocatoreMin)) {
+        return -1; // Il giocatoreMin ha vinto
+      } else if (isPareggio(mosseDisponibiliFor)) {
+        return 0; // Pareggio
+      }
+    
+      let bestScore = -Infinity;
+    
+      for (let i = 0; i < mosseDisponibiliFor.length; i++) {
+        const mossa = mosseDisponibiliFor[i];
+        giocatoreMax.mosse.push(mossa);
+        mosseDisponibiliFor.splice(i, 1);
+    
+        const score = -minimax(giocatoreMin, giocatoreMax, mosseDisponibiliFor);
+    
+        giocatoreMax.mosse.pop();
+        mosseDisponibiliFor.splice(i, 0, mossa);
+    
+        bestScore = Math.max(bestScore, score);
+      }
+    
+      return bestScore;
+    }
+    
+    function mossaBotImpossibile() {
+      const mosseDisponibili = getMosseDisponibili();
+      let bestScore = -Infinity;
+      let bestMove;
+    
+      for (let i = 0; i < mosseDisponibili.length; i++) {
+        const mossa = mosseDisponibili[i];
+        player2.mosse.push(mossa);
+        mosseDisponibili.splice(i, 1);
+    
+        const score = -minimax(player1, player2, mosseDisponibili);
+    
+        player2.mosse.pop();
+        mosseDisponibili.splice(i, 0, mossa);
+    
+        if (score > bestScore) {
+          bestScore = score;
+          bestMove = mossa;
+        }
+      }
+    
+      const botImpossibileCasella = document.getElementById(bestMove.toString());
+      if (botImpossibileCasella) {
+        botImpossibileCasella.click();
+      } else {
+        console.error(`Element with ID ${bestMove} not found.`);
+      }
+    }
+    
+    
+      
+      // Restituisce true se la partita è in pareggio, false altrimenti
+      function isPareggio(mosseDisponibiliFor) {
+        return mosseDisponibiliFor.length === 0;
+      }
+
+      /* ---------------------------------------- */
 
     function gestionePulsantiBoard (event) {
         const casella = event.target;
@@ -219,13 +300,15 @@ const game = (() => {
 
         main.appendChild(sceltePlayer);
         sceltePlayer.appendChild(versusPlayer);
-        sceltePlayer.appendChild(versusBot);
+        sceltePlayer.appendChild(versusBotFacile);
+        sceltePlayer.appendChild(versusBotImpossible);
     }
 
     function giocatoreScelto() {
         // Rimuovi i pulsanti
         versusPlayer.remove();
-        versusBot.remove();
+        versusBotFacile.remove();
+        versusBotImpossible.remove();
     
         const form = document.createElement('form');
         form.style.display = 'flex';
@@ -315,11 +398,12 @@ const game = (() => {
         sceltePlayer.appendChild(form);
     }
 
-    function botScelto() {
+    function botFacileScelto() {
         sceltaFlag = 1;
         // Rimuovi i pulsanti
         versusPlayer.remove();
-        versusBot.remove();
+        versusBotFacile.remove();
+        versusBotImpossible.remove();
     
         const form = document.createElement('form');
         form.style.display = 'flex';
@@ -378,7 +462,6 @@ const game = (() => {
         form.addEventListener('submit', function (event) {
             event.preventDefault();
             
-            ///////////////////////sceltaFlag = 1;
             // Estrai le informazioni dal form
             estraiInformazioniForm(form);
       
@@ -399,11 +482,98 @@ const game = (() => {
 
     }
 
+    function botImpossibileScelto () {
+        sceltaFlag = 2;
+        // Rimuovi i pulsanti
+
+        versusPlayer.remove();
+        versusBotFacile.remove();
+        versusBotImpossible.remove();
+    
+        const form = document.createElement('form');
+        form.style.display = 'flex';
+        form.style.flexDirection = 'column'; 
+
+        // Crea il primo input di testo per il nome del player 1
+        let player1Label = document.createElement('label');
+        player1Label.innerText = "Nome Player 1:";
+        form.appendChild(player1Label);
+        
+        let player1Input = document.createElement('input');
+        player1Input.type = 'text';
+        player1Input.name = 'player1Name';
+        player1Input.setAttribute('required', 'true');
+        form.appendChild(player1Input);
+        
+        // Crea il div per i radio button del segno del player 1
+        let symbolDiv = document.createElement('div');
+        
+        let symbolLabel = document.createElement('label');
+        symbolLabel.innerText = "Segno Player 1:";
+        symbolDiv.appendChild(symbolLabel);
+        
+        let symbolXInput = document.createElement('input');
+        symbolXInput.type = 'radio';
+        symbolXInput.name = 'player1Symbol';
+        symbolXInput.value = 'X';
+        symbolXInput.setAttribute('required', 'true');
+        symbolDiv.appendChild(symbolXInput);
+        
+        let symbolXLabel = document.createElement('label');
+        symbolXLabel.innerText = "X";
+        symbolDiv.appendChild(symbolXLabel);
+        
+        let symbolOInput = document.createElement('input');
+        symbolOInput.type = 'radio';
+        symbolOInput.name = 'player1Symbol';
+        symbolOInput.value = 'O';
+        symbolOInput.setAttribute('required', 'true');
+        symbolDiv.appendChild(symbolOInput);
+        
+        let symbolOLabel = document.createElement('label');
+        symbolOLabel.innerText = "O";
+        symbolDiv.appendChild(symbolOLabel);
+        
+        form.appendChild(symbolDiv);
+        
+        // Crea il pulsante di submit
+        let submitButton = document.createElement('input');
+        submitButton.type = 'submit';
+        submitButton.value = 'Invia';
+        submitButton.setAttribute('target', '_blank');
+        form.appendChild(submitButton);
+
+        
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();
+            
+            // Estrai le informazioni dal form
+            estraiInformazioniForm(form);
+      
+            // Inizializzazione tabelle punti
+            aggiornaInterfaccia(player1, player2);
+
+            // Attiva i pulsanti
+            inizializzaGameBoard ()
+      
+            // Resetta il form
+            form.reset();
+            eliminaSceltaPLayer (form);
+        });
+        
+        // Aggiungi il form alla posizione desiderata nel documento
+        sceltePlayer.appendChild(form);
+        sceltaFlag = 2;
+
+    }
+    
 
     return {
         mossaBotFacile,
+        mossaBotImpossibile,
         player ,
-        getControBot ,
+        getControBotFacile ,
+        getControBotImpossibile ,
         getControPlayer ,
         getNuovoRound ,
         getNuovaPartita,
@@ -413,13 +583,16 @@ const game = (() => {
         inizializzaGameBoard,
         aggiornaInterfaccia,
         giocatoreScelto,
-        botScelto ,
+        botFacileScelto ,
+        botImpossibileScelto,
     };
 })();
 
 game.getControPlayer().addEventListener('click', game.giocatoreScelto);
 
-game.getControBot().addEventListener('click' , game.botScelto);
+game.getControBotFacile().addEventListener('click' , game.botFacileScelto);
+
+game.getControBotImpossibile().addEventListener('click' , game.botImpossibileScelto);
 
 
 game.getCaselleBoard().forEach((casella) => {
@@ -435,6 +608,12 @@ game.getCaselleBoard().forEach((casella) => {
                     if(turnoAttuale === 2) {
                         game.mossaBotFacile();
                     }
+                } else if (sceltaFlag === 2) {
+                    
+                    game.gestionePulsantiBoard(event);
+                    if(turnoAttuale === 2) {
+                        game.mossaBotImpossibile();
+                    }
                 }
                 
             
@@ -445,7 +624,7 @@ game.getCaselleBoard().forEach((casella) => {
 game.getNuovaPartita().addEventListener('click' , function () {
     player1 = {};
     player2= {};
-    mosseDisponibili = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    mosseDisponibiliGl = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     
     modal = document.getElementById('modal');
     modal.style.display = 'none';
@@ -464,7 +643,7 @@ game.getNuovaPartita().addEventListener('click' , function () {
 game.getNuovoRound().addEventListener('click' , function () {
     player1.mosse = [];
     player2.mosse = [];
-    mosseDisponibili = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    mosseDisponibiliGl = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
     modal = document.getElementById('modal');
     modal.style.display = 'none';
@@ -477,3 +656,5 @@ game.getNuovoRound().addEventListener('click' , function () {
     game.aggiornaInterfaccia(player1, player2);
    
 });
+
+
